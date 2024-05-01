c747b8f647d10abc3acfee3b9da687c5f6d12011
Éric GILLES <eric.gilles@etu.umontpellier.fr>
2024-05-01 16:32:58 +0200

    moving shopping-cart

--- Client/mean/src/app/shopping-cart/shopping-cart.component.ts
+++ Client/mean/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../app/header/header.component';
-import { FooterComponent } from '../app/footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-shopping-cart',
