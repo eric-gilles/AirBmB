24b0c4f7485ba40b47eba32ff8e532caef1b6bb3
Éric GILLES <eric.gilles@etu.umontpellier.fr>
2024-05-02 10:05:38 +0200

    fix front & connexion logout todo

--- Client/mean/src/app/header/header.component.ts
+++ Client/mean/src/app/header/header.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { CommonModule } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  userName: string = '';
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.getUserName();
+    
+  }
+  getUserName() {
+    this.userService.getUser().subscribe({next: (data) => {
+      this.userName = data.name;
+    }});
+  }
+
+  isLoggedIn(): boolean {
+    let loggedIn = false;
+    this.userService.getUser().subscribe({
+      next: (data) => {
+        loggedIn = true;
+      },
+      error: (error) => {
+        loggedIn = false;
+      }
+    });
+    return loggedIn;
+  }
+
+}
